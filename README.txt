6c899932b40605a40a4ffd8d4d3b8eb063c8dda7
Jean Villete <jean.villete@gmail.com>
2019-08-04 21:25:31 -0300

    First classes, adding App as component, before moving on and getting it as an arrow function

--- README.txt
+++ README.txt
@@ -4,3 +4,7 @@ $ npm install create-react-app -g/
 $ create-react-app react-complete-guide --scripts-version 1.1.5
 $ cd react-complete-guide
 $ npm start
+
+VS Code extensions [Launch VS Code Quick Open (Ctrl+P), paste the following command, and press enter];
+- ext install joshpeng.sublime-babel-vscode
+- ext install PKief.material-icon-theme
--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Jean',
+        age: 32
+      },
+      {
+        name: 'Gabi',
+        age: 28
+      },
+      {
+        name: 'Dani',
+        age: 28
+      }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // DONT DO THIS: this.state.persons[0].name = 'Jean Villete';
+    this.setState({
+      persons: [
+        {
+          name: 'Jean Villete',
+          age: 32
+        },
+        {
+          name: 'Gabriella',
+          age: 28
+        },
+        {
+          name: 'Dani',
+          age: 28
+        }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi I'm a React app!</h1>
+
+        <button onClick={this.switchNameHandler}>Switch name</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
